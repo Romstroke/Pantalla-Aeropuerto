827fb1a3241286974d244a7da3906f9203203d4a
Romina <rominchuela@gmail.com>
2023-05-11 23:12:10 -0400

    una maravilla, solo falta un mediaquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,3 @@
-
-
-///////// FUNCION PARA ALERTA EN MODAL
-
-function cerrarModal(){
-   document.getElementById('modal').style.display = "none";
-}
-
-var cerrar = document.getElementById('cerrar')
-cerrar.addEventListener('click', cerrarModal);
-
-
 //////// FUNCION QUE ENTREGA HORA ACTUAL
 
 let date = new Date(Date.now());
@@ -19,28 +7,6 @@ const horaFormateada = `${horaActual}`;
 // console.log(fechaActual);
 console.log(horaActual);
 
-
-/////////// FUNCION QUE INYECTA HORA ACTUAL
-
-function injectTime(){
-
-   var hora = document.getElementById('espacioHora');
-
-   var infoHora =
-
-   `
-   <h3>${horaActual}</h3>
-   
-   `
-
-hora.innerHTML = infoHora;
-
-}
-
-injectTime()
-
-
-
 ///////// FUNCION QUE ENTREGA FECHA ACTUAL
 
 const fechaActual = new Date();
@@ -49,71 +15,20 @@ const mes = (fechaActual.getMonth() + 1).toString().padStart(2, '0');
 const anio = fechaActual.getFullYear().toString();
 const fechaFormateada = `${dia}-${mes}-${anio}`;
 console.log(fechaFormateada);
-// Output: "04-05-2023"
 
-//////////////// FUNCION QUE INYECTA FECHA ACTUAL
+/////////// ARRAY VUELOS Y OBJETO DE JS QUE LO RECORRE
 
-function injectDate(){
+var vuelos = ['122','3','45','18','86','22','100','36'];
 
-   var fecha = document.getElementById('espacioFecha');
+   // var random_item = vuelos[Math.floor(Math.random()*vuelos.length)];
 
-   var infoFecha =
+   var randomNumber = Math.floor(Math.random() * 8);
+   console.log(randomNumber)
+   var random_item = vuelos[randomNumber];
 
-        `
-        <h3>${dia}-${mes}-${anio}</h3>        
-        
-        `
-
-fecha.innerHTML = infoFecha;
-
-}
-
-injectDate();
-
-/////////////// ARRAY VUELOS
-
-var vuelos = ['122','3','45','18','86','22','100','36','vaca'];
-
-// var random_item = vuelos[Math.floor(Math.random()*vuelos.length)];
-
-try{
-
-   var random_item = vuelos[Math.round(Math.random()*vuelos.length)];
    console.log(random_item)
 
-}catch (error) {
-   console.error(error);
-}
-
-
-// /////// FUNCION QUE RECORRE LOS VUELOS y los inyecta
-
-
-try{
-
-   function recorrerVuelos(){
-
-      var espacioVuelos = document.getElementById('espacioVuelos');
-   
-        var infoVuelo =
-   
-            `
-               <h3>${random_item}</h3>
-            
-            `
-       
-   
-      console.log(espacioVuelos);
-      espacioVuelos.innerHTML = infoVuelo;
-   }
-   
-   recorrerVuelos()
-
-}catch (error) {
-   console.error(error);
-}
-
-///// ARRAY TERMINALES
+///// OBJETO TERMINALES y recorrido
 
 var terminales = {
    0:'1',
@@ -123,44 +38,234 @@ var terminales = {
    4:'5',
    5:'6',
    6:'7',
-   siete:'8',
+   7:'8',
    8:'9',
-   9:'10'};
+   9:'10'}; // nada impedirá que use un objeto muajajajaja
 
    // var terminales = ['1','2','23','45'];
 
+                                          //// esto es lo mismo que ponerle un 10
+   var random_number = Math.floor(Math.random()*Object.keys(terminales).length); // Object.keys(terminales).length object.keys método, que devuelve una array que representa todas las propiedades enumerables del objeto. Si el objeto está vacío, la longitud de llamada de la array devuelta por Object.keys() debería ser 0.
+   console.log(random_number) //esto me esta dando el largo del array
+   var randomItem = terminales[random_number]; //esto me esta llenando el largo del array con el contenido disponible para cada lugar.
 
-   var randomItem = terminales[Math.floor(Math.random()*Object.keys(terminales).length)]; // object.keys método, que devuelve una array que representa todas las propiedades enumerables del objeto. Si el objeto está vacío, la longitud de llamada de la array devuelta por Object.keys() debería ser 0.
-                                                                                          
+   console.log(randomItem);
 
-   console.log(randomItem)
+///////// ULTRA FUNCION QUE INYECTA TODO (lo que mas se pueda... OSEA EL BODY)
 
-//////// FUNCION QUE RECORRE LOS TERMINALES y los inyecta
+function injectTodo(){
 
-function recorrerTerminales(){
+   document.getElementById('body').innerHTML=
 
-   var espacioTerminal = document.getElementById('espacioTerminal');
-   
-   var infoTerminal=
-
-            `
-            <h3>${randomItem}</h3>
-            
-            `
-
-   console.log(espacioTerminal);
-   espacioTerminal.innerHTML = infoTerminal;
+   `
+   <header id="header" class="header">
+        <div>
+            <h1 class="header__title"><b>Bienvenido a la plataforma <i>Eport</i></b></h1>
+            <p class="header__p">Aquí podrá encontrar la información más reciente<br> acerca de los vuelos en nuestro aeropuerto.</p>
+        </div>
+    </header>
+    <section class="contenedor__time">
+         <div id="espacioHora" class="time">
+            <h2>Hora:</h2>
+            <div>
+               <h3>${horaActual}</h3>
+            </div>
+         </div>
+         <div id="espacioFecha" class="time">
+            <h2>Fecha:</h2>
+            <div>
+               <h3>${dia}-${mes}-${anio}</h3>   
+            </div>  
+         </div>   
+    </section>
+    <section class="contenedor__vuelo">
+        <div >
+            <div id="espacioVuelos">
+               <h2>El vuelo más próximo</h2>
+               <p>Número de vuelo:</p>
+               <h3>${random_item}</h3>
+            </div>
+            <div id="espacioTerminal">
+               <p>Terminal:</p> 
+               <h3>${randomItem}</h3>
+            </div>
+        </div>
+    </section>
+    <div id="modal" class="modal">
+      <div class="modal__contenido">
+         <h1 class="modal__aviso">RECUERDE</h1>
+         <h3 class="modal__mensaje">
+         Estimado pasajero, se le recuerda que para abordar el avión debe tener
+         <span>en mano su pasaporte y su boleto de embarque</span>. Manténgase
+         atento a los avisos mediante esta plataforma.<br>
+         </h3>
+         <button id="cerrar">
+            <p>OK GRACIAS</p>
+         </button>
+      </div>
+   </div>
+   `
 
 }
 
-recorrerTerminales()
+injectTodo()
+
+///////// FUNCION PARA CERRAR EL MODAL
+
+function cerrarModal(){
+   document.getElementById('modal').style.display = "none";
+}
+
+var cerrar = document.getElementById('cerrar')
+cerrar.addEventListener('click', cerrarModal);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////                                                  /////////////////////////////////////////
+///////////////////       funciones de inyección por separado        /////////////////////////////////////////
+///////////////////                                                  /////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+///// INYECCION EN HEADER (por qué no me toma el tag ni el class?????) porque esos esperan varios elementos que vienen en un array, y tendria que ponerle cual de todos quiero.
+
+   // document.getElementById('header').innerHTML=
+   // `
+   // <div>
+   // <h1 class="header__title"><b>Bienvenido a la plataforma <i>Eport</i></b></h1>
+   // <p class="header__p">Aquí podrá encontrar la información más reciente<br> acerca de los vuelos en nuestro aeropuerto.</p>
+   // </div>
+   // `
+
+///// FUNCION QUE INYECTA EL MODAL
+
+// function injectContentModal(){
+//    var modal = document.getElementById('modal');
+
+//    var infoModal =
+   
+//    `
+//    <div class="modal__contenido">
+//    <h1 class="modal__aviso">RECUERDE</h1>
+//    <h3 class="modal__mensaje">
+//      Estimado pasajero, se le recuerda que para abordar el avión debe tener
+//      <span>en mano su pasaporte y su boleto de embarque</span>. Manténgase
+//      atento a los avisos mediante esta plataforma.<br>
+//    </h3>
+//    <button id="cerrar">
+//      <p>OK GRACIAS</p>
+//    </button>
+//    </div>
+//    `
+
+//    modal.innerHTML = infoModal;
+// }
+
+// injectContentModal()
+
+/////////// FUNCION QUE INYECTA HORA ACTUAL
+
+// function injectTime(){
+
+//    var hora = document.getElementById('espacioHora');
+
+//    var infoHora =
+
+//    `
+//    <h2>Hora:</h2>
+//    <div>
+//        <h3>${horaActual}</h3>
+//    </div>
+   
+//    `
+
+// hora.innerHTML = infoHora;
+
+// }
+
+// injectTime()
+
+/////////// FUNCION QUE INYECTA FECHA ACTUAL
+
+// function injectDate(){
+
+//    var fecha = document.getElementById('espacioFecha');
+
+//    var infoFecha =
+
+//         `
+//         <h2>Fecha:</h2>
+//         <div>
+//            <h3>${dia}-${mes}-${anio}</h3>   
+//         </div>     
+        
+//         `
+
+// fecha.innerHTML = infoFecha;
+
+// }
+
+// injectDate();
+
+// /////// FUNCION QUE INYECTA LOS VUELOS
+
+   // function recorrerVuelos(){
+
+   //    var espacioVuelos = document.getElementById('espacioVuelos');
+   
+   //      var infoVuelo =
+   
+   //          `
+   //          <h2>El vuelo más próximo</h2>
+   //          <p>Número de vuelo:</p>
+   //          <h3>${random_item}</h3>
+            
+   //          `
+       
+   
+   //    console.log(espacioVuelos);
+   //    espacioVuelos.innerHTML = infoVuelo;
+   // }
+   
+   // recorrerVuelos()
+
+
+//////// FUNCION QUE INYECTA LOS TERMINALES
+
+// function recorrerTerminales(){
+
+//    var espacioTerminal = document.getElementById('espacioTerminal'); 
+   
+//    var infoTerminal=
+
+//             `
+//             <p>Terminal:</p> 
+//             <h3>${randomItem}</h3>
+            
+//             `
+
+//    console.log(espacioTerminal);
+//    espacioTerminal.innerHTML = infoTerminal;
+
+// }
+
+// recorrerTerminales()
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////                                                  /////////////////////////////////////////
+///////////////////   cosas que dejo para venir a estudiar después   /////////////////////////////////////////
+///////////////////                                                  /////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// var infoModal = document.getElementById('modal')
+// infoModal.addEventListener('load',injectContentModal)
+
+// window.addEventListener("load", function(event) {
+//    console.log("'Todos los recursos terminaron de cargar!");
+//  });
 
 
 // var aviso = "Pasajero, se le recuerda que para abordar el avión debe tener\n";
@@ -170,9 +275,9 @@ recorrerTerminales()
 
 // alert(aviso);
 
-   // var tareasObjeto = {'cero':'uno','dos':'tres'};
-  // var tareasArr = ['cero','uno','dos'];
-  // console.log(typeof tareasObjeto)
+//    var tareasObjeto = {'cero':'uno','dos':'tres'};
+//   var tareasArr = ['cero','uno','dos'];
+//   console.log(typeof tareasObjeto)
 // console.log(typeof tareasArr)
 
 
